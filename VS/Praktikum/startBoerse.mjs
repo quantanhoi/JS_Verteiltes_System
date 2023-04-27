b61b94ab7a2d8508c0fedb163f8b85e6e8746b6a
quantanhoi <thieuquangtrung1999@gmail.com>
2023-04-27 10:11:38 +0200

    update

--- VS/Praktikum/startBoerse.mjs
+++ VS/Praktikum/startBoerse.mjs
@@ -1,7 +1,29 @@
 import {Bank, firstBank} from './Bank.mjs';
 import { Boerse, b1 } from './Boerse.mjs';
 import { Wertpapier, MSFT, LSFT } from './Wertpapier.mjs';
-b1.addWerpapier(MSFT, 100);
-console.log(b1.wertpapiers);
+import { createInterface } from 'readline';
+import { stdin, stdout } from 'process';
 b1.connectToBank(firstBank);
-b1.sendData(MSFT, 50);
+const rl = createInterface({
+    input: stdin,
+    output: stdout
+  });  
+process.stdin.setRawMode(true);
+process.stdin.resume();
+console.log("Press e key to send data to the bank or q to quit");
+rl.input.on('data', (key) => {
+    const keyPressed = key.toString();
+    if (keyPressed === 'q') {
+      console.log('Exiting...');
+      rl.close();
+    } else if (keyPressed === 'e' || keyPressed === 'E') {
+      b1.addWerpapier(MSFT, 100);
+      console.log(b1.wertpapiers);
+      b1.sendData(MSFT, 50);
+    }
+  });
+  
+
+rl.on('close', () => {
+    process.exit(0);
+})
